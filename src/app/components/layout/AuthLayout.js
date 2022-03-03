fd9101440a222e7eb264564e3a9651a1e8480be2
Claudio Marai <claudio.marai@devinterface.com>
2022-03-03 17:13:07 +0100

    add footer

--- src/app/components/layout/AuthLayout.js
+++ src/app/components/layout/AuthLayout.js
@@ -1,26 +1,33 @@
+
 import React from 'react'
+import BaseFooter from 'app/components/layout/BaseFooter'
 import { Container, Image, Col, Row, Card } from 'react-bootstrap'
 import '../../../css/authLayout/auth.css'
 
 const AuthLayout = ({ children }) => {
   return (
-    <Container>
-      <Card className='card-auth center-div shadow-box'>
-        <Row>
-          <Col lg={6} md={12} sm={12}>
-            <div className='card-body text-center'>
-              <Image rounded='true' src='/images/articoliesocial-1.svg' alt='Articoliesocial Subscription' />
-            </div>
-          </Col>
-          <Col lg={6} md={10} sm={12}>
-            <div className='card-body text-center'>
-              <Image className='logo m-b-40' src='/images/logo-startersaas.svg' alt='Articoliesocial Logo' />
-              {children}
-            </div>
-          </Col>
-        </Row>
-      </Card>
-    </Container>
+    <div className='d-flex flex-column' id='h100'>
+      <div id='page-content' className='auth-content'>
+        <Container>
+          <Card className='card-auth center-div shadow-box'>
+            <Row>
+              <Col lg={6} md={12} sm={12}>
+                <div className='card-body text-center'>
+                  <Image rounded='true' src='/images/articoliesocial-1.svg' alt='Articoliesocial Subscription' />
+                </div>
+              </Col>
+              <Col lg={6} md={10} sm={12}>
+                <div className='card-body text-center'>
+                  <Image className='logo m-b-40' src='/images/logo-startersaas.svg' alt='Articoliesocial Logo' />
+                  {children}
+                </div>
+              </Col>
+            </Row>
+          </Card>
+        </Container>
+      </div>
+      <BaseFooter />
+    </div>
   )
 }
 
