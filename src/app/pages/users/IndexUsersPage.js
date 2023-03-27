40bb657631aafa2d8aa2b0e6b2b241cf0f520219
Stefano Mancini <stefano.mancini@devinterface.com>
2023-03-27 13:04:47 +0200

    fixed edit user

--- src/app/pages/users/IndexUsersPage.js
+++ src/app/pages/users/IndexUsersPage.js
@@ -96,110 +96,260 @@ const IndexUsersPage = ({ user }) => {
   return (
     <div className="max-width">
       <Modal show={createUserPopup} onHide={() => setCreateUserPopup(false)}>
-        <div className='popup-crea-gruppo p-4'>
+        <div className="popup-crea-gruppo p-4">
           <div>
-            <div className='fw-bold mb-2'>{t("createUsersPage.name")}</div>
-            <input onChange={(event) => { handleUserNameInput(event) }} type={'text'} className='w-100-perc popup-crea-gruppo-input' placeholder="..." />
+            <div className="fw-bold mb-2">{t("createUsersPage.name")}</div>
+            <input
+              onChange={(event) => {
+                handleUserNameInput(event);
+              }}
+              type={"text"}
+              className="w-100-perc popup-crea-gruppo-input"
+              placeholder="..."
+            />
           </div>
           <div>
-            <div className='fw-bold mb-2'>{t("createUsersPage.surname")}</div>
-            <input onChange={(event) => { handleUserSurnameInput(event) }} type={'text'} className="w-100-perc popup-crea-gruppo-input" placeholder="..." />
+            <div className="fw-bold mb-2">{t("createUsersPage.surname")}</div>
+            <input
+              onChange={(event) => {
+                handleUserSurnameInput(event);
+              }}
+              type={"text"}
+              className="w-100-perc popup-crea-gruppo-input"
+              placeholder="..."
+            />
           </div>
           <div>
-            <div className='fw-bold mb-2'>{t("createUsersPage.email")}</div>
-            <input onChange={(event) => { handleUserEmailInput(event) }} type={'text'} className="w-100-perc popup-crea-gruppo-input" placeholder="..." />
+            <div className="fw-bold mb-2">{t("createUsersPage.email")}</div>
+            <input
+              onChange={(event) => {
+                handleUserEmailInput(event);
+              }}
+              type={"text"}
+              className="w-100-perc popup-crea-gruppo-input"
+              placeholder="..."
+            />
           </div>
           <Dropdown>
-            <Dropdown.Toggle className="w-100-perc text-dark bg-white">{userData.language}</Dropdown.Toggle>
+            <Dropdown.Toggle className="w-100-perc text-dark bg-white">
+              {userData.language}
+            </Dropdown.Toggle>
             <Dropdown.Menu className="w-100-perc text-dark bg-white text-center">
-              <Dropdown.Item onClick={() => setUserData({ ...userData, language: "en" })}>en</Dropdown.Item>
-              <Dropdown.Item onClick={() => setUserData({ ...userData, language: "it" })}>it</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => setUserData({ ...userData, language: "en" })}
+              >
+                en
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => setUserData({ ...userData, language: "it" })}
+              >
+                it
+              </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
           <Dropdown>
-            <Dropdown.Toggle className="w-100-perc text-dark bg-white">{userRole(userData.role)}</Dropdown.Toggle>
+            <Dropdown.Toggle className="w-100-perc text-dark bg-white">
+              {userRole(userData.role)}
+            </Dropdown.Toggle>
             <Dropdown.Menu className="w-100-perc text-dark bg-white text-center">
-              <Dropdown.Item onClick={() => setUserData({ ...userData, role: "user" })}>User</Dropdown.Item>
-              <Dropdown.Item onClick={() => setUserData({ ...userData, role: "admin" })}>Admin</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => setUserData({ ...userData, role: "user" })}
+              >
+                User
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => setUserData({ ...userData, role: "admin" })}
+              >
+                Admin
+              </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
-          <div className='d-flex justify-content-center gap-5'>
-            <div onClick={() => {
-              setCreateUserPopup(false); setUserData({
-                name: "",
-                surname: "",
-                email: "",
-                language: "en",
-                role: "user"
-              });
-
-            }} className="group-cancel-button d-flex justify-content-center align-items-center">{t("createUsersPage.cancel")}</div>
-            <div onClick={() => { setCreateUserPopup(false); createUser() }} className='create-group-button d-flex justify-content-center align-items-center'>{t("createUsersPage.save")}</div>
+          <div className="d-flex justify-content-center gap-5">
+            <div
+              onClick={() => {
+                setCreateUserPopup(false);
+                setUserData({
+                  name: "",
+                  surname: "",
+                  email: "",
+                  language: "en",
+                  role: "user",
+                });
+              }}
+              className="group-cancel-button d-flex justify-content-center align-items-center"
+            >
+              {t("createUsersPage.cancel")}
+            </div>
+            <div
+              onClick={() => {
+                setCreateUserPopup(false);
+                createUser();
+              }}
+              className="create-group-button d-flex justify-content-center align-items-center"
+            >
+              {t("createUsersPage.save")}
+            </div>
           </div>
         </div>
       </Modal>
       <Modal show={deleteUserPopup}>
-        <div className='delete-popup '>
-          <div className='text-center m-3'>{t("indexUsersPage.deleteUser")}?</div>
-          <div className='d-flex justify-content-center gap-5 mb-3'>
-            <div onClick={() => { setDeleteUserPopup(false); deleteUser(userId); setUserId("") }} className="group-cancel-button d-flex justify-content-center align-items-center">{t("indexUsersPage.yes")}</div>
-            <div onClick={() => { setDeleteUserPopup(false); setUserId("") }} className="group-cancel-button d-flex justify-content-center align-items-center">{t("indexUsersPage.no")}</div>
+        <div className="delete-popup ">
+          <div className="text-center m-3">
+            {t("indexUsersPage.deleteUser")}?
+          </div>
+          <div className="d-flex justify-content-center gap-5 mb-3">
+            <div
+              onClick={() => {
+                setDeleteUserPopup(false);
+                deleteUser(userId);
+                setUserId("");
+              }}
+              className="group-cancel-button d-flex justify-content-center align-items-center"
+            >
+              {t("indexUsersPage.yes")}
+            </div>
+            <div
+              onClick={() => {
+                setDeleteUserPopup(false);
+                setUserId("");
+              }}
+              className="group-cancel-button d-flex justify-content-center align-items-center"
+            >
+              {t("indexUsersPage.no")}
+            </div>
           </div>
         </div>
       </Modal>
       <h1>{t("indexUsersPage.users")}</h1>
-      <div className='dashboard-box mt-5 h-auto'>
-        <div className='d-flex justify-content-end mb-4'>
-          <div className='mt-3 create-group-button d-flex justify-content-center align-items-center' onClick={() => setCreateUserPopup(true)}>{t("buttonUsers.createUser")}</div>
+      <div className="dashboard-box mt-5 h-auto">
+        <div className="d-flex justify-content-end mb-4">
+          <div
+            className="mt-3 create-group-button d-flex justify-content-center align-items-center"
+            onClick={() => setCreateUserPopup(true)}
+          >
+            {t("buttonUsers.createUser")}
+          </div>
         </div>
         <Table responsive bordered>
           <thead>
             <tr className="d-none d-md-table-row">
-              <th className='col-1 text-start fs-5' style={{ borderLeft: "0px", borderRight: "0px" }}>{t("indexUsersPage.name")}</th>
-              <th className='col-1 text-start fs-5' style={{ borderLeft: "0px", borderRight: "0px" }}>{t("indexUsersPage.surname")}</th>
-              <th className='col-1 text-start fs-5' style={{ borderLeft: "0px", borderRight: "0px" }} id="code-group">{t("indexUsersPage.email")}</th>
-              <th className='col-1 text-start fs-5' style={{ borderLeft: "0px", borderRight: "0px" }} id="delete-group">{t("indexUsersPage.role")}</th>
-              <th className='col-1 text-start fs-5' style={{ borderLeft: "0px", borderRight: "0px" }} id="delete-group">{t("indexUsersPage.actions")}</th>
+              <th
+                className="col-1 text-start fs-5"
+                style={{ borderLeft: "0px", borderRight: "0px" }}
+              >
+                {t("indexUsersPage.name")}
+              </th>
+              <th
+                className="col-1 text-start fs-5"
+                style={{ borderLeft: "0px", borderRight: "0px" }}
+              >
+                {t("indexUsersPage.surname")}
+              </th>
+              <th
+                className="col-1 text-start fs-5"
+                style={{ borderLeft: "0px", borderRight: "0px" }}
+                id="code-group"
+              >
+                {t("indexUsersPage.email")}
+              </th>
+              <th
+                className="col-1 text-start fs-5"
+                style={{ borderLeft: "0px", borderRight: "0px" }}
+                id="delete-group"
+              >
+                {t("indexUsersPage.role")}
+              </th>
+              <th
+                className="col-1 text-start fs-5"
+                style={{ borderLeft: "0px", borderRight: "0px" }}
+                id="delete-group"
+              >
+                {t("indexUsersPage.actions")}
+              </th>
             </tr>
           </thead>
           <tbody>
             {data ? (
               <>
-                {
-                  data.data.map((element, i) => (
-                    <tr key={`element.name-${i}`} className="text-secondary d-flex flex-column d-md-table-row">
-                      <div className="d-md-none fs-6 fw-bold pb-0">{t("indexUsersPage.name")}:</div>
-                      <td className='text-start border-0 text-break'>{element.name}</td>
-                      <div className="d-md-none fs-6 fw-bold pb-0">{t("indexUsersPage.surname")}:</div>
-                      <td className='text-start border-0 text-break'>{element.surname}</td>
-                      <div className="d-md-none fs-6 fw-bold pb-0">{t("indexUsersPage.email")}:</div>
-                      <td className='text-start border-0 text-break'>{element.email}</td>
-                      <div className="d-md-none fs-6 fw-bold pb-0">{t("indexUsersPage.role")}:</div>
-                      <td className='text-start border-0 text-break'>{element.role}</td>
-                      <div className="d-md-none fs-6 fw-bold pb-0">{t("indexUsersPage.actions")}:</div>
-                      <td className='d-flex justify-content-start align-items-center' style={{ borderBottom: "0px", borderRight: "0px", borderLeft: "0px" }}>
-                        {user.name != element.name ? (
-                          < Link className='me-2 group-button-edit text-decoration-none' to={`/user/${element.id}`}>{t("buttonUsers.edit")}</Link>
-                        ) :
-                          <Link className='me-2 group-button-edit text-decoration-none' to={`/user/edit`}>{t("buttonUsers.edit")}</Link>
-                        }
-                        {
-                          user.accountOwner && user.name != element.name ?
-                            (
-                              <div className='group-button-delete' onClick={() => { setDeleteUserPopup(true); setUserId(element.id) }}>{t("buttonUsers.delete")}</div>
-                            ) : ""
-                        }
-                      </td>
-                    </tr>
-                  ))
-                }
+                {data.data.map((element, i) => (
+                  <tr
+                    key={`element.name-${i}`}
+                    className="text-secondary d-flex flex-column d-md-table-row"
+                  >
+                    <div className="d-md-none fs-6 fw-bold pb-0">
+                      {t("indexUsersPage.name")}:
+                    </div>
+                    <td className="text-start border-0 text-break">
+                      {element.name}
+                    </td>
+                    <div className="d-md-none fs-6 fw-bold pb-0">
+                      {t("indexUsersPage.surname")}:
+                    </div>
+                    <td className="text-start border-0 text-break">
+                      {element.surname}
+                    </td>
+                    <div className="d-md-none fs-6 fw-bold pb-0">
+                      {t("indexUsersPage.email")}:
+                    </div>
+                    <td className="text-start border-0 text-break">
+                      {element.email}
+                    </td>
+                    <div className="d-md-none fs-6 fw-bold pb-0">
+                      {t("indexUsersPage.role")}:
+                    </div>
+                    <td className="text-start border-0 text-break">
+                      {element.role}
+                    </td>
+                    <div className="d-md-none fs-6 fw-bold pb-0">
+                      {t("indexUsersPage.actions")}:
+                    </div>
+                    <td
+                      className="d-flex justify-content-start align-items-center"
+                      style={{
+                        borderBottom: "0px",
+                        borderRight: "0px",
+                        borderLeft: "0px",
+                      }}
+                    >
+                      {user.name != element.name ? (
+                        <Link
+                          className="me-2 group-button-edit text-decoration-none"
+                          to={`/edit-user/${element.id}`}
+                        >
+                          {t("buttonUsers.edit")}
+                        </Link>
+                      ) : (
+                        <Link
+                          className="me-2 group-button-edit text-decoration-none"
+                          to={`/user/edit`}
+                        >
+                          {t("buttonUsers.edit")}
+                        </Link>
+                      )}
+                      {user.accountOwner && user.name != element.name ? (
+                        <div
+                          className="group-button-delete"
+                          onClick={() => {
+                            setDeleteUserPopup(true);
+                            setUserId(element.id);
+                          }}
+                        >
+                          {t("buttonUsers.delete")}
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                    </td>
+                  </tr>
+                ))}
               </>
-            ) : ""
-            }
+            ) : (
+              ""
+            )}
           </tbody>
         </Table>
       </div>
-    </div >
+    </div>
   );
 };
 export default IndexUsersPage;
